67aa858a5213c8b8b584d204297ba42ef81d30d5
agamsanghera <agamsanghera911@gmail.com>
2024-08-11 01:13:14 +0530

    Added comments

--- CalendarPort.py
+++ CalendarPort.py
@@ -12,7 +12,10 @@ a["Location"]=a["Meeting Patterns"].str.split('|',expand=True)[3].str.split(" - 
 a=a.drop(columns=["Instructional Format","Delivery Mode","Section","Meeting Patterns"])
 #rename columns as expected input for google calendar
 a=a.rename(columns={"Course Listing":"Subject","Instructor":"Description"})
+#make the classes show as events in the specified timeframe
 a["All Day Event"]="False"
+#display all the columns in the head
 pd.set_option('display.max_columns', None)
 print(a.head())
+#save as CSV which can be uploaded to google calendar
 a.to_csv("GoogleCalendar.csv",index=False)
